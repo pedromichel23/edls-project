c607a5c9dac37a3fb7a37fe315b0c1210593386e
Pedro Michel <pedromichel23@gmail.com>
2024-03-10 18:09:52 -0700

    create project

---
+++ edls.go
@@ -0,0 +1,28 @@
+package main
+
+// file types
+const (
+	fileRegular int = iota
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileLink
+)
+
+// file extensions
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	gz  = ".gz"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpg = ".jpg"
+	gif = ".gif"
+)
+
+func main() {
+
+}
